a74f825b58c9a58728aa26065fc90fb3572ac6c8
Roman Bitarovský <bitarovskyroman@gmail.com>
2022-03-16 21:13:43 +0100

    faza 3 done ??

--- src/Blockchain.java
+++ src/Blockchain.java
@@ -33,27 +33,48 @@ public class Blockchain {
             return new UTXOPool(uPool);
          }
     }
+
+    // premenne pre moj vlatny blockchain
+    private TransactionPool txPool;
+    private HashMap<ByteArrayWrapper, BlockNode> blockChain;
+    private BlockNode highestNode;
+
     /**
      * vytvor prázdny blockchain iba s Genesis blokom. Predpokladajme, že
      * {@code genesisBlock} je platný blok
      */
     public Blockchain(Block genesisBlock) {
         // IMPLEMENTOVAŤ
+        blockChain = new HashMap<>();
+        this.txPool = new TransactionPool();
+
+        UTXOPool UTXOPool = new UTXOPool();
+        addCoinbaseTx(genesisBlock, UTXOPool);
+
+        BlockNode genesisNode = new BlockNode(genesisBlock, null, UTXOPool);
+        highestNode = genesisNode;
+
+        blockChain.put(new ByteArrayWrapper((genesisBlock.getHash())), genesisNode);
+
+
     }
 
     /** Získaj maximum height blok */
     public Block getBlockAtMaxHeight() {
         // IMPLEMENTOVAŤ
+        return highestNode.b;
     }
 
     /** Získaj UTXOPool na ťaženie nového bloku na vrchu max height blok */
     public UTXOPool getUTXOPoolAtMaxHeight() {
         // IMPLEMENTOVAŤ
+        return highestNode.getUTXOPoolCopy();
     }
 
     /** Získaj pool transakcií na vyťaženie nového bloku */
     public TransactionPool getTransactionPool() {
         // IMPLEMENTOVAŤ
+        return txPool;
     }
 
     /**
@@ -70,10 +91,59 @@ public class Blockchain {
      */
     public boolean blockAdd(Block block) {
         // IMPLEMENTOVAŤ
+
+        // zober predosli blok ak nieje null
+        byte[] previousBlock = block.getPrevBlockHash();
+        if (previousBlock == null){
+            return false;
+        }
+
+        // zober parenta ak nieje null
+        BlockNode parentBlockNode = blockChain.get(new ByteArrayWrapper(previousBlock));
+        if (parentBlockNode == null) {
+            return false;
+        }
+
+        // zober UTXO pool predchadzajuceho bloku
+        HandleTxs handlerTxs = new HandleTxs(parentBlockNode.getUTXOPoolCopy());
+        Transaction[] txs = block.getTransactions().toArray(new Transaction[0]);
+
+        // skontroluj txs predchadzajuceho bloku
+        Transaction[] validTxs = handlerTxs.handler(txs);
+        if (validTxs.length != txs.length) {
+            return false;
+        }
+
+        if (parentBlockNode.height + 1 <= highestNode.height - CUT_OFF_AGE){
+            return false;
+        }
+
+        UTXOPool utxoPool = handlerTxs.UTXOPoolGet();
+
+        // coinbase tx teda vymajnovany BTC treba do bloku pridat ako prvu transakciu
+        addCoinbaseTx(block, utxoPool);
+
+        BlockNode node = new BlockNode(block, parentBlockNode, utxoPool);
+
+        blockChain.put(new ByteArrayWrapper(block.getHash()), node);
+
+        if (parentBlockNode.height + 1 > highestNode.height){
+            highestNode = node;
+        }
+
+        return true;
     }
 
     /** Pridaj transakciu do transakčného poolu */
     public void transactionAdd(Transaction tx) {
         // IMPLEMENTOVAŤ
+        txPool.addTransaction(tx);
+    }
+
+    // funkcia na pridanie coinbase tx
+    private void addCoinbaseTx(Block block, UTXOPool utxoPool){
+        Transaction coinbase = block.getCoinbase();
+        UTXO utxoCoinbase = new UTXO(coinbase.getHash(), 0);
+        utxoPool.addUTXO(utxoCoinbase, coinbase.getOutput(0));
     }
 }
